58eeadada47c5dc509a452d8b87ef0ece2de1042
12204284 <12204284@office.inha.ac.kr>
2022-05-12 22:41:40 +0900

    w10 hw

--- 10주차과제.cpp
+++ 10주차과제.cpp
@@ -10,19 +10,14 @@
 using namespace cv;
 using namespace std;
 
-void cvHarrisCorner() {
-	Mat img = imread("lab8/ship.png");
-	if (img.empty()) {
-		cout << "empty image!\n";
-		exit(-1);
-	}
+Mat cvHarrisCorner(Mat &img) {
 	resize(img, img, Size(500, 500), 0, 0, INTER_CUBIC);
 
 	Mat gray;
 	cvtColor(img, gray, CV_BGR2GRAY);
 
 	Mat harr;
-	cornerHarris(gray, harr, 2, 3, 0.05, BORDER_DEFAULT);
+	cornerHarris(gray, harr, 3, 5, 0.02, 4);
 	normalize(harr, harr, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
 
 	Mat harr_abs;
@@ -30,23 +25,21 @@ void cvHarrisCorner() {
 
 	int thresh = 125;
 	Mat result = img.clone();
+	int i = 0;
 	for (int y = 0; y < harr.rows; y++) {
 		for (int x = 0; x < harr.cols; x++) {
-			if ((int)harr.at<float>(y, x) > thresh)
+			if ((int)harr.at<float>(y, x) > thresh) {
 				circle(result, Point(x, y), 7, Scalar(255, 0, 255), 0, 4, 0);
+				i++;
+			}
 		}
 	}
-	
-	imshow("source im", img);
-	imshow("Harris im", harr_abs);
-	imshow("Target im", result);
-	waitKey(0);
-	cvDestroyAllWindows();
-
+	cout << i << endl;
+	return result;
 }
 
-void cvFeaturesSIFT() {
-	Mat img = imread("lab8/church.jpg", 1);
+Mat cvFeaturesSIFT(Mat& img) {
+	//Mat img = imread("lab8/church.jpg", 1);
 	resize(img, img, Size(512, 512), 0, 0, INTER_CUBIC);
 
 	Mat gray;
@@ -56,17 +49,38 @@ void cvFeaturesSIFT() {
 	vector<KeyPoint> keypoints;
 	detector->detect(gray, keypoints);
 
-	cout << "normal SIFT kp num = " << keypoints.size() << endl;
+	cout << keypoints.size() << endl;
 
 	Mat result;
 	drawKeypoints(img, keypoints, result);
-	imshow("sift result", result);
+	return result;
+	//imshow("sift result", result);
 	//waitKey(0);
 	//cvDestroyAllWindows();
 }
 
-void cvBlobDetection() {
-	Mat img = imread("lab8/coin.png", IMREAD_COLOR);
+Mat warpPers(Mat& src) {
+	resize(src, src, Size(512, 512), 0, 0, INTER_CUBIC);
+	Mat dst;
+
+	Point2f src_p[4], dst_p[4];
+
+	src_p[0] = Point2f(0, 0);
+	src_p[1] = Point2f(512, 0);
+	src_p[2] = Point2f(0, 512);
+	src_p[3] = Point2f(512, 512);
+
+	dst_p[0] = Point2f(50, 50);
+	dst_p[1] = Point2f(450, 50);
+	dst_p[2] = Point2f(0, 512);
+	dst_p[3] = Point2f(512, 512);
+
+	Mat pers_mat = getPerspectiveTransform(src_p, dst_p);
+	warpPerspective(src, dst, pers_mat, Size(512, 512));
+	return dst;
+}
+
+Mat cvBlobDetection(Mat& img) {
 	//Mat img = imread("lab8/circle.jpg", IMREAD_COLOR);
 
 	SimpleBlobDetector::Params params;
@@ -92,75 +106,64 @@ void cvBlobDetection() {
 	drawKeypoints(img, ketpoints, result,
 		Scalar(255, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-	imshow("keypoints", result);
-	waitKey(0);
-	cvDestroyAllWindows();
+
+	return result;
 }
 
-void warpPers() {
-	Mat src = imread("lab8/church.jpg", 1);
-	resize(src, src, Size(512, 512), 0, 0, INTER_CUBIC);
-	Mat dst;
+void hw1() {
+	Mat img = imread("lab8/coin.png", IMREAD_COLOR);
+	Mat rst = cvBlobDetection(img);
+	imshow("keypoints", rst);
+	waitKey(0);
+	cvDestroyAllWindows();
 
-	Point2f src_p[4], dst_p[4];
+}
 
-	src_p[0] = Point2f(0, 0);
-	src_p[1] = Point2f(512, 0);
-	src_p[2] = Point2f(0, 512);
-	src_p[3] = Point2f(512, 512);
+void hw2() {
+	Mat tri_img = imread("lab8/triangle.jpg");
+	Mat squ_img = imread("lab8/square.jpg");
+	Mat pen_img = imread("lab8/pentagon.jpg");
+	Mat hex_img = imread("lab8/hexagon.jpg");
 
-	dst_p[0] = Point2f(0, 0);
-	dst_p[1] = Point2f(512, 0);
-	dst_p[2] = Point2f(0, 512);
-	dst_p[3] = Point2f(412, 412);
+	cout << "triangle corner = ";
+	Mat tri_dst = cvHarrisCorner(tri_img);
+	cout << "square corner = ";
+	Mat squ_dst = cvHarrisCorner(squ_img);
+	cout << "pentagon corner = ";
+	Mat pen_dst = cvHarrisCorner(pen_img);
+	cout << "hexagon corner = ";
+	Mat hex_dst = cvHarrisCorner(hex_img);
 
-	Mat pers_mat = getPerspectiveTransform(src_p, dst_p);
-	warpPerspective(src, dst, pers_mat, Size(512, 512));
-	imshow("result", dst);
-	waitKey();
+	imshow("tri_dst", tri_dst);
+	imshow("squ_dst", squ_dst);
+	imshow("pen_dst", pen_dst);
+	imshow("hex_dst", hex_dst);
+	waitKey(0);
+	cvDestroyAllWindows();
 }
 
 void hw3() {
 	Mat src = imread("lab8/church.jpg", 1);
 	resize(src, src, Size(512, 512), 0, 0, INTER_CUBIC);
 
+	cout << "SIFT original img = ";
+	Mat original_SIFT_result = cvFeaturesSIFT(src);
+
 	Mat brig_dst;
-	add(src, Scalar(100), brig_dst);
+	add(src, Scalar(100,100,100), brig_dst);
 
-	Mat dst;
+	Mat dst = warpPers(brig_dst); //투시변환
 
-	Point2f src_p[4], dst_p[4];
+	cout << "SIFT trans img = ";
+	Mat trans_SIFT_result = cvFeaturesSIFT(dst);
 
-	src_p[0] = Point2f(0, 0);
-	src_p[1] = Point2f(512, 0);
-	src_p[2] = Point2f(0, 512);
-	src_p[3] = Point2f(512, 512);
-
-	dst_p[0] = Point2f(70, 70);
-	dst_p[1] = Point2f(512, 0);
-	dst_p[2] = Point2f(0, 512);
-	dst_p[3] = Point2f(480, 480);
-
-	Mat pers_mat = getPerspectiveTransform(src_p, dst_p);
-	warpPerspective(src, brig_dst, pers_mat, Size(512, 512));
-
-
-	Mat gray;
-	cvtColor(dst, gray, CV_BGR2GRAY);
-
-	Ptr<SiftFeatureDetector> detector = SiftFeatureDetector::create();
-	vector<KeyPoint> keypoints;
-	detector->detect(gray, keypoints);
-
-	cout << "changed SIFT kp num = " << keypoints.size() << endl;
-
-	Mat result;
-	drawKeypoints(dst, keypoints, result);
-	imshow("hw3 result", result);
+	imshow("hw3 original_SIFT_result", original_SIFT_result);
+	imshow("hw3 trans_SIFT_result", trans_SIFT_result);
 	waitKey();
 }
 
-int main() {
-	//cvFeaturesSIFT();
-	//hw3();
+
+
+int main(){
+	hw2();
 }
